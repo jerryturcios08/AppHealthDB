c7709f3086008f39f50cbda8feabaaaa9ad6a2d9
David S. Li <ds.li@outlook.com>
2018-02-16 23:00:36 -0500

    Added enums and new Address Object

--- src/hackcewit2018/ClientProfile.java
+++ src/hackcewit2018/ClientProfile.java
@@ -1,5 +1,7 @@
 package hackcewit2018;
 
+import java.util.Date;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -13,10 +15,102 @@ package hackcewit2018;
 public class ClientProfile {
 
     private String name;
+    private Address address;
+    
+    private Date dateOfBirth;
+    private Gender gender;
+    private String ssn;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(Date dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public Gender getGender() {
+        return gender;
+    }
+
+    public void setGender(Gender gender) {
+        this.gender = gender;
+    }
+
+    public String getSsn() {
+        return ssn;
+    }
+
+    public void setSsn(String ssn) {
+        this.ssn = ssn;
+    }
+    
+    public ClientProfile(String name) {
+        this.name = name;
+        //this.address = address;
+        
+    }
+    
+}
+
+class Address {
     private String address;
     private String city;
     private State state;
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public State getState() {
+        return state;
+    }
+
+    public void setState(State state) {
+        this.state = state;
+    }
+
+    public int getZip() {
+        return zip;
+    }
+
+    public void setZip(int zip) {
+        this.zip = zip;
+    }
     private int zip;
+    
+    public Address(String address, String city, State state, int zip) {
+        this.address = address;
+        this.city = city;
+        this.state = state;
+        this.zip = zip;
+    }
+}
+
+enum Gender {
+    M,
+    F,
+    T
 }
 
 enum State {
