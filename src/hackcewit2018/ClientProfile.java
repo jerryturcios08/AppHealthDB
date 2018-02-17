ebfad538d945d6ffb47a8ead6a5ae575664e9ed0
David S. Li <ds.li@outlook.com>
2018-02-16 22:44:59 -0500

    First commit of new repo

---
+++ src/hackcewit2018/ClientProfile.java
@@ -0,0 +1,75 @@
+package hackcewit2018;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author dsli
+ */
+public class ClientProfile {
+
+    private String name;
+    private String address;
+    private String city;
+    private State state;
+    private int zip;
+}
+
+enum State {
+    AL, 
+    AK,
+    AZ,
+    AR,
+    CA,
+    CO,
+    CT,
+    DE,
+    FL,
+    GA,
+    HI,
+    ID,
+    IL,
+    IN,
+    IA,
+    KS,
+    KY,
+    LA,
+    ME,
+    MD,
+    MA,
+    MI,
+    MN,
+    MS,
+    MO,
+    MT,
+    NE,
+    NV,
+    NH,
+    NJ,
+    NM,
+    NY,
+    NC,
+    ND,
+    OH,
+    OK,
+    OR,
+    PA,
+    RI,
+    SC,
+    SD,
+    TN,
+    TX,
+    UT,
+    VT,
+    VA,
+    WA,
+    WV,
+    WI,
+    WY,
+    // Special areas
+    DC
+}
