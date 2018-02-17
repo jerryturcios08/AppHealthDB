4a7ece61c18225c099bc99f35de1ac0dcf5e8578
Your Name <you@example.com>
2018-02-17 00:44:15 -0500

    Add login screen

--- src/hackcewit2018/HackCEWIT2018.java
+++ src/hackcewit2018/HackCEWIT2018.java
@@ -8,37 +8,68 @@ package hackcewit2018;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
+
+import java.awt.*;
 
 /**
  *
  * @author dsli
+ * @author jerryturcios08
  */
 public class HackCEWIT2018 extends Application {
     
     @Override
     public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-            
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
-        });
-        
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-        
-        Scene scene = new Scene(root, 300, 250);
-        
-        primaryStage.setTitle("Hello World!");
+        GridPane grid = new GridPane();
+        grid.setAlignment(Pos.CENTER);
+        grid.setHgap(10);
+        grid.setVgap(10);
+        grid.setPadding(new Insets(25, 25, 25, 25));
+
+        Scene scene = new Scene(grid, 800, 500);
+
+        primaryStage.setTitle("App Health DB");
         primaryStage.setScene(scene);
         primaryStage.show();
+
+        Label scenetitle = new Label("App Health Databases 2018");
+        scenetitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 16));
+        grid.add(scenetitle, 0, 0, 2, 1);
+
+        Label username = new Label("User Name:");
+        grid.add(username, 0, 1);
+        TextField userTextField = new TextField();
+        grid.add(userTextField, 1, 1);
+
+        Label password = new Label("Password:");
+        grid.add(password, 0, 2);
+        PasswordField pwBox = new PasswordField();
+        grid.add(pwBox, 1, 2);
+
+        Button btn = new Button("Sign in");
+        HBox hbBtn = new HBox(10);
+        hbBtn.setAlignment(Pos.BOTTOM_RIGHT);
+        hbBtn.getChildren().add(btn);
+        grid.add(hbBtn, 1, 4);
+
+        final Label actiontarget = new Label();
+        grid.add(actiontarget, 1, 6);
+
+        // TODO: Remove this line and add a message box for validation input
+        btn.setOnAction(e -> System.out.println("Sign In button was pressed!"));
     }
 
     /**
